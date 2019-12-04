54c7bd861d75ac223039b850d9f08bca0476f572
Michael Knoll <mknoll@fearless.tech>
2019-12-04 15:36:39 -0500

    Updates

---
+++ precache-manifest.5c701519975a2e6e66383e4401173315.js
@@ -0,0 +1,286 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0139eaaa0ec3dc52e3869d0505beb4f",
+    "url": "/USDS-SSA-SPL/index.html"
+  },
+  {
+    "revision": "626328c6f4529ffd9a3a",
+    "url": "/USDS-SSA-SPL/static/css/2.b4fa1422.chunk.css"
+  },
+  {
+    "revision": "ff263cb2e0906a3e1b39",
+    "url": "/USDS-SSA-SPL/static/css/main.49604d22.chunk.css"
+  },
+  {
+    "revision": "626328c6f4529ffd9a3a",
+    "url": "/USDS-SSA-SPL/static/js/2.f21a9e44.chunk.js"
+  },
+  {
+    "revision": "ff263cb2e0906a3e1b39",
+    "url": "/USDS-SSA-SPL/static/js/main.ee46b436.chunk.js"
+  },
+  {
+    "revision": "a793b17baa4f7fe302cc",
+    "url": "/USDS-SSA-SPL/static/js/runtime-main.1649fd75.js"
+  },
+  {
+    "revision": "a3ee995ea9d6a796976456b54af5d8ab",
+    "url": "/USDS-SSA-SPL/static/media/angle-arrow-down-hover.a3ee995e.svg"
+  },
+  {
+    "revision": "b03111e98c11810da72da5ba38993989",
+    "url": "/USDS-SSA-SPL/static/media/angle-arrow-down-primary-hover.b03111e9.svg"
+  },
+  {
+    "revision": "64f4e9687ab10d5205dfb2a13314ddeb",
+    "url": "/USDS-SSA-SPL/static/media/angle-arrow-down-primary.64f4e968.svg"
+  },
+  {
+    "revision": "c3c72a6ee199232be52daa761c4a7582",
+    "url": "/USDS-SSA-SPL/static/media/angle-arrow-down.c3c72a6e.svg"
+  },
+  {
+    "revision": "5304e70f45e70a9c305f181f929b4052",
+    "url": "/USDS-SSA-SPL/static/media/angle-arrow-up-primary-hover.5304e70f.svg"
+  },
+  {
+    "revision": "7f380f792390566450e98a901f3f3c22",
+    "url": "/USDS-SSA-SPL/static/media/angle-arrow-up-primary.7f380f79.svg"
+  },
+  {
+    "revision": "6ca32ab4864d8b010f0308c08772e946",
+    "url": "/USDS-SSA-SPL/static/media/arrow-both.6ca32ab4.svg"
+  },
+  {
+    "revision": "134ee96f77fb18c6d879bf962912a085",
+    "url": "/USDS-SSA-SPL/static/media/arrow-down.134ee96f.svg"
+  },
+  {
+    "revision": "1483e05534c83b4db269d418ea27fc2d",
+    "url": "/USDS-SSA-SPL/static/media/arrow-right.1483e055.svg"
+  },
+  {
+    "revision": "4a1065171514ee5b1535885a0a7b817c",
+    "url": "/USDS-SSA-SPL/static/media/close.4a106517.svg"
+  },
+  {
+    "revision": "66e36b49ebeb82d7e6e385bf77eb3fdd",
+    "url": "/USDS-SSA-SPL/static/media/correct8-alt.66e36b49.svg"
+  },
+  {
+    "revision": "944de30f0d6c95c2aa544f8b9cac8f27",
+    "url": "/USDS-SSA-SPL/static/media/correct8.944de30f.svg"
+  },
+  {
+    "revision": "1d70e405146ea70e69de06359952e843",
+    "url": "/USDS-SSA-SPL/static/media/correct9.1d70e405.svg"
+  },
+  {
+    "revision": "8773ca7e457bfc1b3de3d87df927edda",
+    "url": "/USDS-SSA-SPL/static/media/error.8773ca7e.svg"
+  },
+  {
+    "revision": "48f4f10f8b2bc89625a53b23578f8711",
+    "url": "/USDS-SSA-SPL/static/media/external-link-alt-hover.48f4f10f.svg"
+  },
+  {
+    "revision": "18b41ab22f64fda9a9c1da6ca3bd8773",
+    "url": "/USDS-SSA-SPL/static/media/external-link-alt.18b41ab2.svg"
+  },
+  {
+    "revision": "f09ec8586b4d786847e5229617754e17",
+    "url": "/USDS-SSA-SPL/static/media/external-link-hover.f09ec858.svg"
+  },
+  {
+    "revision": "bdcbd439a464ff3037dad45b44af345c",
+    "url": "/USDS-SSA-SPL/static/media/external-link.bdcbd439.svg"
+  },
+  {
+    "revision": "37c061f74cb30ea358a1981cc34c3904",
+    "url": "/USDS-SSA-SPL/static/media/facebook25.37c061f7.svg"
+  },
+  {
+    "revision": "544cee01f2a29ef3fd128c78b8727f1f",
+    "url": "/USDS-SSA-SPL/static/media/hero.544cee01.png"
+  },
+  {
+    "revision": "f6fcba349ae758f6bfe3ccbf521ef8c4",
+    "url": "/USDS-SSA-SPL/static/media/info.f6fcba34.svg"
+  },
+  {
+    "revision": "9f5d0a16e9ffe3c205b06f6d9bfda434",
+    "url": "/USDS-SSA-SPL/static/media/logo.9f5d0a16.svg"
+  },
+  {
+    "revision": "3a8907c945b4ea384ad0b99150847706",
+    "url": "/USDS-SSA-SPL/static/media/merriweather-bold-webfont.3a8907c9.ttf"
+  },
+  {
+    "revision": "65d5dea57619d14d4e012b3a25550207",
+    "url": "/USDS-SSA-SPL/static/media/merriweather-bold-webfont.65d5dea5.eot"
+  },
+  {
+    "revision": "6d33d1693d8c3e674020a003b03d5aef",
+    "url": "/USDS-SSA-SPL/static/media/merriweather-bold-webfont.6d33d169.woff2"
+  },
+  {
+    "revision": "b6b7444ffb6c03617a6a3c337965ce78",
+    "url": "/USDS-SSA-SPL/static/media/merriweather-bold-webfont.b6b7444f.woff"
+  },
+  {
+    "revision": "5568fe0d4ab09cd47227a2baade5fac2",
+    "url": "/USDS-SSA-SPL/static/media/merriweather-italic-webfont.5568fe0d.woff"
+  },
+  {
+    "revision": "5b09f19714acef0663643ff49294a777",
+    "url": "/USDS-SSA-SPL/static/media/merriweather-italic-webfont.5b09f197.eot"
+  },
+  {
+    "revision": "9830783aaa859c742823f3fefd60b867",
+    "url": "/USDS-SSA-SPL/static/media/merriweather-italic-webfont.9830783a.woff2"
+  },
+  {
+    "revision": "ad5dd583afab8f453645b057029586bf",
+    "url": "/USDS-SSA-SPL/static/media/merriweather-italic-webfont.ad5dd583.ttf"
+  },
+  {
+    "revision": "726e8a5e407de4813fba4998cf946761",
+    "url": "/USDS-SSA-SPL/static/media/merriweather-light-webfont.726e8a5e.ttf"
+  },
+  {
+    "revision": "8e9df39cafa62b65262a85908e33ec66",
+    "url": "/USDS-SSA-SPL/static/media/merriweather-light-webfont.8e9df39c.woff2"
+  },
+  {
+    "revision": "c370c7b61d28a26a1c8107def5d57b7d",
+    "url": "/USDS-SSA-SPL/static/media/merriweather-light-webfont.c370c7b6.woff"
+  },
+  {
+    "revision": "f423e689e0bdaa0f7b0587785f334e56",
+    "url": "/USDS-SSA-SPL/static/media/merriweather-light-webfont.f423e689.eot"
+  },
+  {
+    "revision": "18fb7572812a600eeb86ecbdeb3c3064",
+    "url": "/USDS-SSA-SPL/static/media/merriweather-regular-webfont.18fb7572.woff2"
+  },
+  {
+    "revision": "479fc84c654874d031d50f8e878f2139",
+    "url": "/USDS-SSA-SPL/static/media/merriweather-regular-webfont.479fc84c.ttf"
+  },
+  {
+    "revision": "8af54a984d2993508bf270e9204d1860",
+    "url": "/USDS-SSA-SPL/static/media/merriweather-regular-webfont.8af54a98.woff"
+  },
+  {
+    "revision": "a1e02af03345e22ea9f0c3c56cf1b932",
+    "url": "/USDS-SSA-SPL/static/media/merriweather-regular-webfont.a1e02af0.eot"
+  },
+  {
+    "revision": "8816fd01d020f9eed45e4f17ed128e15",
+    "url": "/USDS-SSA-SPL/static/media/minus-alt.8816fd01.svg"
+  },
+  {
+    "revision": "5a7e9c3749351ab3c46b4b239fbbf2bd",
+    "url": "/USDS-SSA-SPL/static/media/minus.5a7e9c37.svg"
+  },
+  {
+    "revision": "1c26be1a8f4ed2eb58ab7074ccc20e83",
+    "url": "/USDS-SSA-SPL/static/media/plus-alt.1c26be1a.svg"
+  },
+  {
+    "revision": "eb771fb87ddbd32d9e5f66f0564b5abd",
+    "url": "/USDS-SSA-SPL/static/media/plus.eb771fb8.svg"
+  },
+  {
+    "revision": "ad09b3218da26c977631ada8a2daab96",
+    "url": "/USDS-SSA-SPL/static/media/rss25.ad09b321.svg"
+  },
+  {
+    "revision": "37d9028ee1429b8d8001f41568d35375",
+    "url": "/USDS-SSA-SPL/static/media/search-alt.37d9028e.svg"
+  },
+  {
+    "revision": "069823480e15b7b391de5f268e12194b",
+    "url": "/USDS-SSA-SPL/static/media/search.06982348.svg"
+  },
+  {
+    "revision": "74f174d071369e0df8566c8a3013e001",
+    "url": "/USDS-SSA-SPL/static/media/sourcesanspro-bold-webfont.74f174d0.woff"
+  },
+  {
+    "revision": "86937a00b9e6bd153ddb2d25a296896d",
+    "url": "/USDS-SSA-SPL/static/media/sourcesanspro-bold-webfont.86937a00.ttf"
+  },
+  {
+    "revision": "cc7343dca32ce1c4de62e430c449b216",
+    "url": "/USDS-SSA-SPL/static/media/sourcesanspro-bold-webfont.cc7343dc.eot"
+  },
+  {
+    "revision": "db08924457dfce83611a4392af58de04",
+    "url": "/USDS-SSA-SPL/static/media/sourcesanspro-bold-webfont.db089244.woff2"
+  },
+  {
+    "revision": "1c766c2ac93653f97310ac420ef6aa84",
+    "url": "/USDS-SSA-SPL/static/media/sourcesanspro-italic-webfont.1c766c2a.woff"
+  },
+  {
+    "revision": "95366d5c901f8de3fbc6aef7421e0da7",
+    "url": "/USDS-SSA-SPL/static/media/sourcesanspro-italic-webfont.95366d5c.eot"
+  },
+  {
+    "revision": "ab48e5edfe4dc666d1aeba338fc078b8",
+    "url": "/USDS-SSA-SPL/static/media/sourcesanspro-italic-webfont.ab48e5ed.woff2"
+  },
+  {
+    "revision": "c5a1649c9d410b426fca73e29c4f20a5",
+    "url": "/USDS-SSA-SPL/static/media/sourcesanspro-italic-webfont.c5a1649c.ttf"
+  },
+  {
+    "revision": "36b2454850951a572647d682c969937f",
+    "url": "/USDS-SSA-SPL/static/media/sourcesanspro-light-webfont.36b24548.eot"
+  },
+  {
+    "revision": "3920c044810625353fedd3e074553422",
+    "url": "/USDS-SSA-SPL/static/media/sourcesanspro-light-webfont.3920c044.woff"
+  },
+  {
+    "revision": "5110c10b6fcf515fc776cd1b62aab226",
+    "url": "/USDS-SSA-SPL/static/media/sourcesanspro-light-webfont.5110c10b.ttf"
+  },
+  {
+    "revision": "91533bcffe7d49099b98dea3e7093217",
+    "url": "/USDS-SSA-SPL/static/media/sourcesanspro-light-webfont.91533bcf.woff2"
+  },
+  {
+    "revision": "01c34960c01c24ac996e1348d25c54eb",
+    "url": "/USDS-SSA-SPL/static/media/sourcesanspro-regular-webfont.01c34960.ttf"
+  },
+  {
+    "revision": "699e5f09daf577ae815ddc7b920f8e24",
+    "url": "/USDS-SSA-SPL/static/media/sourcesanspro-regular-webfont.699e5f09.woff2"
+  },
+  {
+    "revision": "d2e3cf3e91620fa02db78122b43b856f",
+    "url": "/USDS-SSA-SPL/static/media/sourcesanspro-regular-webfont.d2e3cf3e.woff"
+  },
+  {
+    "revision": "d8ab7c53981c7d8247c52e2caab2721a",
+    "url": "/USDS-SSA-SPL/static/media/sourcesanspro-regular-webfont.d8ab7c53.eot"
+  },
+  {
+    "revision": "2eb47a3ff8f1770a58340b17f745b977",
+    "url": "/USDS-SSA-SPL/static/media/success.2eb47a3f.svg"
+  },
+  {
+    "revision": "0168b3b6acafc97c978ba91010b3d455",
+    "url": "/USDS-SSA-SPL/static/media/twitter16.0168b3b6.svg"
+  },
+  {
+    "revision": "4ba1d198d9487ee97e4cf851ed2de1f3",
+    "url": "/USDS-SSA-SPL/static/media/warning.4ba1d198.svg"
+  },
+  {
+    "revision": "bededcffe4cfd671d5c63db5d04c9374",
+    "url": "/USDS-SSA-SPL/static/media/youtube15.bededcff.svg"
+  }
+]);
